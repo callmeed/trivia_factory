82e8707ca4de1270a301abde4590730ee603ddbe
Callmeed <erik.dungan@gmail.com>
2017-10-13 11:34:39 -0700

    math question

--- lib/trivia_factory.rb
+++ lib/trivia_factory.rb
@@ -6,16 +6,21 @@ module TriviaFactory
     QUESTION_TYPES = [:true_false, :multiple_choice, :fill_in_the_blank].freeze
     ANSWER_TYPES = [:choice_index, :boolean, :string, :integer]
 
+    attr_accessor :label
+    attr_accessor :question_type
+    attr_accessor :choices
+    attr_accessor :answer_type
+    attr_accessor :answer
+
     def initialize
       @label = "Who won the MLB World Series in 2016?"
       @question_type = :multiple_choice
       @choices = ["San Francisco Giants", "Chicago Cubs", "Cleveland Indians", "Golden State Warriors"]
       @answer_type = :choice_index
       @answer = 1
-      payload
     end
 
-    def payload
+    def to_h
       {
         label: @label,
         question_type: @question_type,
@@ -28,6 +33,19 @@ module TriviaFactory
     class << self
       def generate
         question = TriviaFactory::Question.new
+      end
+
+      def math(max = 100)
+        question = TriviaFactory::Question.new
+        rng = Random.new
+        first = rng.rand(max)
+        second = rng.rand(max)
+        question.label = "#{first} + #{second} = _____?"
+        question.question_type = :fill_in_the_blank
+        question.answer_type = :integer
+        question.answer = first + second
+        question.choices = []
+        question
       end
     end
   end
